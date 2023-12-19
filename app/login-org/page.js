d8f7de09dcd9c34e0e74de013b79449156c38ce1
yasuogg <sohaibben52@gmail.com>
2023-12-19 16:39:04 +0000

    login page v-last without backend

--- app/login-org/page.js
+++ app/login-org/page.js
@@ -5,22 +5,27 @@ export default function login() {
   return (
     <>
     <Logo/>
-    <div className='flex justify-center items-center text-gray-500'>
-    <div className='flex-row text-gray justify-center items-center w-100% h-100% '>
+    <div className='flex justify-evenly'> 
+    <div className='flex justify-center items-center lg:flex lg:justify-start lg:p-32  w-50% h-50% text-gray-500'>
+    <div className='flex-row text-gray justify-center items-center '>
       <div className='flex-row justify-center items-center'>
+      <h1 className='text-6xl font-bold text-[#faaa1f] text-center'>Dash
+      <br/><p className='text-[#0053B5]'>Connect</p></h1>
         <div className='flex-col p-1'>
     <label className='flex p-2'>Username</label>
-    <input className='shadow appearance-none border rounded w-50% py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:text-[#0053B5]'/>
+    <input className='shadow appearance-none border-2 rounded w-40% text-2xl py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:text-[#0053B5] rounded-full'/>
     </div>
     <div className='flex-col p-1'>
-    <label className='flex p-2'>password</label>
-    <input type='password' className='shadow appearance-none border rounded w-50% py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:text-[#0053B5]'/>
+    <label className='flex p-2'>Password</label>
+    <input type='password' className='shadow appearance-none border-2 rounded text-2xl w-50% py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:text-[#0053B5] rounded-full'/>
     </div>
     </div>
     <div className='flex pt-7 justify-center items-center'>
-    <button className='flex  bg-[#0053B5] hover:bg-sky-700 text-white font-bold py-4 px-16 rounded-full'>Login</button>
+    <button className='flex text-2xl  bg-[#0053B5] hover:bg-sky-600 text-white font-bold py-3 px-20 rounded-full'>Login</button>
     </div>
     </div>
+    </div>
+    <Image src="/dash.png" className='hidden lg:block' width={650} height={100}/>
     </div>
     </>
   )
