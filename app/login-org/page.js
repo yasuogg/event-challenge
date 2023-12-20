3e97d429db093a2f22a82414b1cb13048ffdd1f9
yasuogg <sohaibben52@gmail.com>
2023-12-20 11:19:16 +0000

    change photo

--- app/login-org/page.js
+++ app/login-org/page.js
@@ -5,19 +5,19 @@ export default function login() {
   return (
     <>
     <Logo/>
-    <div className='flex justify-evenly'> 
+    <div className='flex justify-around'> 
     <div className='flex justify-center items-center lg:flex lg:justify-start lg:p-32  w-50% h-50% text-gray-500'>
     <div className='flex-row text-gray justify-center items-center '>
       <div className='flex-row justify-center items-center'>
-      <h1 className='text-6xl font-bold text-[#faaa1f] text-center'>Dash
+      <h1 className=' text-7xl lg:text-8xl font-bold text-[#faaa1f] text-center'>Dash
       <br/><p className='text-[#0053B5]'>Connect</p></h1>
         <div className='flex-col p-1'>
     <label className='flex p-2'>Username</label>
-    <input className='shadow appearance-none border-2 rounded w-40% text-2xl py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:text-[#0053B5] rounded-full'/>
+    <input className=' focus:border-blue-500 duration-500 shadow appearance-none border-2 rounded w-40% text-2xl lg:text-3xl py-3 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:text-[#0053B5] rounded-full'/>
     </div>
     <div className='flex-col p-1'>
     <label className='flex p-2'>Password</label>
-    <input type='password' className='shadow appearance-none border-2 rounded text-2xl w-50% py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:text-[#0053B5] rounded-full'/>
+    <input type='password' className=' focus:border-blue-500 duration-500 shadow appearance-none border-2 rounded text-2xl lg:text-3xl w-50% py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:text-[#0053B5] rounded-full'/>
     </div>
     </div>
     <div className='flex pt-7 justify-center items-center'>
@@ -25,8 +25,8 @@ export default function login() {
     </div>
     </div>
     </div>
-    <Image src="/dash.png" className='hidden lg:block' width={650} height={100}/>
+    <Image  src="/dash.png" className='  hidden lg:block' width={900} height={50}/>
     </div>
-    </>
+    </>  
   )
 }
